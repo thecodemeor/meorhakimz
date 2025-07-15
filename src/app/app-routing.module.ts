5ea1634ec366ac885b6b947c1b841bda863ab0e7
thecodemeor <meorhakimz@gmail.com>
2025-07-15 10:47:08 +0800

    new UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//--- Page Routes ---//
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
+// ---  Angular Package  --- //
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppComponent } from 'src/app/app.component';
+
+// --- Extra --- //
+
+// --- Extra Component --- //
+// import { LogoComponent } from 'src/assets/components/logo.component';
+import { ButtonComponent } from 'src/assets/components/button.component';
+import { InputComponent } from 'src/assets/components/input.component';
+
+import { SpaceRingCursor } from 'src/assets/components/curify/space-ring.component';
+
+// --- Page List --- //
+import { HomeComponent } from 'src/app/pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+
+        // LogoComponent,
+        ButtonComponent,
+        InputComponent,
+        SpaceRingCursor
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        CommonModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
